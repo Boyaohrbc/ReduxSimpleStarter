c5265c0b5e1e0360a4525488ddf3ef66fbb364ed
Bo <boyao2005@gmail.com>
2016-06-15 12:24:51 -0700

    section2

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{title: 'Javascript: The Good part', pages: 100},
+		{title: 'The Dark Tower', pages: 200},
+		{title: 'Into the Wild', pages: 300},
+		{title: 'Walalalala', pages: 50000}
+	]
+}
